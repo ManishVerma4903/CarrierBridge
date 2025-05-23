5317c612f89bfef6311b1bef0df8e82d3fb48f96
Manish Verma <manish8006406786@gmail.com>
2025-05-23 16:07:22 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    // origin:'http://localhost:5173',
-    origin:'https://carrier-bridge-zsq3.vercel.app',
+    origin:['http://localhost:5173',"https://carrier-bridge-zsq3.vercel.app"],
     credentials:true
 }
 
